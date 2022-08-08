3a5e19694bf2e479c4988489d560ffb48c9850be
Gabi-b612 <20km095@esisalama.org>
2022-08-08 23:33:12 +0200

    Acquisition of the doctor identity

--- docteur.c
+++ docteur.c
@@ -12,4 +12,44 @@ typedef struct docteur
     char specialisation[50];
 } Doc;
 
+void save_doctor(Doc *p)
+{   
+    int len_number, i;
 
+    printf("Entrez le prénom du docteur : ");
+    scanf("%20s", (*p).prenom);
+    // 
+    printf("Entrez le nom du docteur : ");
+    scanf("%20s", (*p).nom);
+    // 
+    printf("Entrez le postnom du docteur : ");
+    scanf("%20s", (*p).postnom);
+    // 
+    //
+    printf("Entrez le numero de telephone du docteur :");
+    scanf("%s", (*p).telephone);
+    len_number = strlen((*p).telephone);
+    while (len_number != 10 || (*p).telephone[0] != '0')
+    {
+        printf("\nImpossible d'enregistre votre numero....\n");
+        printf("Verifier que votre numero commence par 08 ou 09 et qu'il a 10 chiffres\n");
+        printf("\nEntrez a nouveau votre numero :");
+        scanf("%s", (*p).telephone);
+        len_number = strlen((*p).telephone);
+    }
+    if (len_number == 10)
+    {
+        while ((*p).telephone[1] != '8' && (*p).telephone[1] != '9')
+        {
+            printf("\nImpossible d'enregistre votre numero....\n");
+            printf("Verifier que votre numero commence par 08 ou 09 et qu'il a 10 chiffres\n");
+            printf("\nEntrez a nouveau votre numero :");
+            scanf("%s", (*p).telephone);
+        }   
+    }
+}
+void Adding(Doc rep[], int indice)
+{
+    printf("\nEnregistrement d'un docteur....\n");
+    save_doctor(&rep[indice]);
+}
